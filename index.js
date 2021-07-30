c0ac74852dd078eba016ac1a89746880dcdf58b0
Pedro Alexandre <pedroalexandre.bastos@hotmail.com>
2021-07-30 19:21:38 -0300

    Back-end: Routes and Middlewares

--- index.js
+++ index.js
@@ -10,14 +10,25 @@ let posts = [
     },
 ]
 
-//Routes
 app.get("/all", (req, res) => {
     res.json(JSON.stringify(posts));
-})
+});
 
-app.post("/new", (req, res) => { })
+app.post("/new", express.json(),  (req, res) => { 
+    let id = generateID();
+    let title = req.body.title;
+    let description = req.body.description;
+
+    posts.push({id, title, description});
+    
+    res.send("Post added");
+});
 
 
 app.listen(port, () => {
     console.log("Server running on port", port);
-})
+});
+
+function generateID(){
+    return Math.random().toString(36).substr(2, 9);
+}
